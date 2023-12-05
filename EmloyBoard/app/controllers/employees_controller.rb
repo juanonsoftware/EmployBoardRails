90421436af70df9c0cd2f8a0029bfc0de7839457
juanonsoftware <juanonsoftware@gmail.com>
2023-12-05 15:15:50 +0700

    Adding avatar upload

--- EmloyBoard/app/controllers/employees_controller.rb
+++ EmloyBoard/app/controllers/employees_controller.rb
@@ -65,6 +65,6 @@ class EmployeesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def employee_params
-      params.require(:employee).permit(:full_name, :email)
+      params.require(:employee).permit(:full_name, :email, :avatar)
     end
 end
--- EmloyBoard/app/models/employee.rb
+++ EmloyBoard/app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    has_one_attached :avatar
+
+    def image_as_thumbnail
+        if avatar.attached?
+            avatar.variant(resize_to_limit: [350, 350]).processed          
+        end
+    end
 end
